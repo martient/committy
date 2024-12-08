ccf86163338677c6660bdb7ca6f376552dc3c7fd
Arnaud (Martient) Leherpeur <arnaud.leherpeur@taultek.com>
2024-12-08 20:41:04 +0100

    feat(commit-type): add security type

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ pub const SENTRY_DSN: &str = "https://994880aa262e93af0026738f64a5576c@o45080862
 
 pub const COMMIT_TYPES: &[&str] = &[
     "feat", "fix", "build", "chore", "ci", "cd", "docs", "perf", "refactor", "revert", "style",
-    "test",
+    "test", "security"
 ];
 
 pub const MAX_SHORT_DESCRIPTION_LENGTH: usize = 150;
@@ -10,4 +10,4 @@ pub const MAX_SHORT_DESCRIPTION_LENGTH: usize = 150;
 pub const MAJOR_REGEX: &str = r"(?im)^(breaking change:|feat(?:\s*\([^)]*\))?!:)";
 pub const MINOR_REGEX: &str = r"(?im)^feat(?:\s*\([^)]*\))?:";
 pub const PATCH_REGEX: &str =
-    r"(?im)^(fix|docs|style|refactor|perf|test|chore|ci|cd|build|revert)(?:\s*\([^)]*\))?:";
+    r"(?im)^(fix|docs|style|refactor|perf|test|chore|ci|cd|build|revert|security)(?:\s*\([^)]*\))?:";
