90e1bc0ce196e2cb8ad709f9699375e75f9405d6
Arnaud (Martient) Leherpeur <arnaud.leherpeur@taultek.com>
2025-04-12 12:11:07 +0200

    config: add new type

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,13 @@
 pub const COMMIT_TYPES: &[&str] = &[
     "feat", "fix", "build", "chore", "ci", "cd", "docs", "perf", "refactor", "revert", "style",
-    "test", "security",
+    "test", "security", "config",
 ];
 
 pub const MAX_SHORT_DESCRIPTION_LENGTH: usize = 150;
 
 pub const MAJOR_REGEX: &str = r"(?im)^(breaking change:|feat(?:\s*\([^)]*\))?!:)";
 pub const MINOR_REGEX: &str = r"(?im)^feat(?:\s*\([^)]*\))?:";
-pub const PATCH_REGEX: &str = r"(?im)^(fix|docs|style|refactor|perf|test|chore|ci|cd|build|revert|security)(?:\s*\([^)]*\))?:";
+pub const PATCH_REGEX: &str = r"(?im)^(fix|docs|style|refactor|perf|test|chore|ci|cd|build|revert|security|config)(?:\s*\([^)]*\))?:";
 
 use anyhow::Result;
 use chrono::{DateTime, FixedOffset};
